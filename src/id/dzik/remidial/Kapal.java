43b466c81edfb91723238ada66309b73f977da82
dzikrialifa <Dzikrialif99@gmail.com>
2018-11-04 20:36:40 +0700

    class untuk pelayaran dengan Kapal

--- src/id/dzik/remidial/Kapal.java
+++ src/id/dzik/remidial/Kapal.java
@@ -7,7 +7,8 @@ package id.dzik.remidial;
 public class Kapal {
     private String nama;
     private int harga;
-
+    private int kuota;
+    
     public Kapal() {
     }
 
@@ -16,12 +17,21 @@ public class Kapal {
     }
     public void setNama(String nama) {
         this.nama = nama;
+        setHarga();
     }
     public int getHarga() {
         return harga;
     }
-    public void setHarga(int harga) {
-        this.harga = harga;
+    public void setHarga() {
+        if (this.nama.equalsIgnoreCase("Garuda Indonesia")) {
+            this.harga = 1000000;
+        }else if (this.nama.equalsIgnoreCase("Sriwijaya Air")) {
+            this.harga = 750000;
+        }else if (this.nama.equalsIgnoreCase("Lion Air")) {
+            this.harga = 500000;
+        }else{
+            System.out.println("Menu not found");
+        }
     }
     
     
