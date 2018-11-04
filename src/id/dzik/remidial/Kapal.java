39534ffd90b16fa2fd9c2ae7a5a7e214421a4039
HP <HP@DESKTOP-DZ>
2018-11-04 20:00:13 +0700

    class untuk pelayaran dengan Kapal

---
+++ src/id/dzik/remidial/Kapal.java
@@ -0,0 +1,32 @@
+/*
+*   DZIKRI ALIF AB
+*   Email : dzikrialif99@gmail.com
+*   Hawlet Packard
+*/
+package id.dzik.remidial;
+public class Kapal {
+    private String nama;
+    private int harga;
+
+    public Kapal() {
+    }
+
+    public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+    
+    
+    
+    
+}
