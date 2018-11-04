349783d56f095ce3e42e17515552e6ab1578add4
dzikrialifa <Dzikrialif99@gmail.com>
2018-11-04 21:04:16 +0700

    class transportasi dengan kapal

--- src/id/dzik/remidial/Kapal.java
+++ src/id/dzik/remidial/Kapal.java
@@ -7,7 +7,7 @@ package id.dzik.remidial;
 public class Kapal {
     private String nama;
     private int harga;
-    private int kuota;
+    private int orang;
     
     public Kapal() {
     }
@@ -23,17 +23,40 @@ public class Kapal {
         return harga;
     }
     public void setHarga() {
-        if (this.nama.equalsIgnoreCase("Garuda Indonesia")) {
-            this.harga = 1000000;
-        }else if (this.nama.equalsIgnoreCase("Sriwijaya Air")) {
-            this.harga = 750000;
-        }else if (this.nama.equalsIgnoreCase("Lion Air")) {
-            this.harga = 500000;
+        if (this.nama.equalsIgnoreCase("KM Senopati")) {
+            this.harga = 150000;
+        }else if (this.nama.equalsIgnoreCase("KM Arjuna")) {
+            this.harga = 100000;
+        }else if (this.nama.equalsIgnoreCase("KM Basudewa")) {
+            this.harga = 50000;
         }else{
             System.out.println("Menu not found");
         }
     }
-    
+
+    public int getOrang() {
+        return orang;
+    }
+
+    public void setOrang(int orang) {
+        this.orang = orang;
+    }
+    public void setHargaOrg(){
+        if (this.nama.equalsIgnoreCase("Dewasa")) {
+            this.harga = 1000000;
+        }else if (this.nama.equalsIgnoreCase("Anak")) {
+            this.harga = 750000;
+        }else{
+            System.out.println("Menu not found");
+        }
+    }
+      public String info(){
+          String info = "";
+          info += "Memilih Kapal\t: "+getNama()+"\n";
+          info += "Kuota Kapal\t\t: "+getOrang()+"\n";
+          info += "Harga Tiket\t\t: "+getHarga()+"\n";
+          return info;
+      }
     
     
     
