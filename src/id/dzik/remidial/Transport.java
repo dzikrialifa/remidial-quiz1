5b468b4fa4d499af679a2f809b996622f7e7db72
dzikrialifa <Dzikrialif99@gmail.com>
2018-11-04 21:41:31 +0700

    class transportasi

--- src/id/dzik/remidial/Transport.java
+++ src/id/dzik/remidial/Transport.java
@@ -18,14 +18,22 @@ public class Transport {
     public void setPesawat(Pesawat pesawat) {
         this.pesawat = pesawat;
     }
-    public int hargaTotal(){
+    public int hargaTotalKapal(){
         int total = this.kapal.getHarga() * this.kapal.getOrang();
                 return total;
     }
+    public int hargaTotalPesawat(){
+        int total = this.pesawat.getHarga() * this.pesawat.getOrang();
+        return total;
+    }
     public void info(){
         String info = "";
+        System.out.println("=========================================");
         System.out.println("Transportasi Kapal\t: "+this.kapal.info());
-        System.out.println("Transportasi Pesawat\t: ");
-        System.out.println("Total harga Armada\t: "+hargaTotal());
+        System.out.println("Transportasi Pesawat\t: "+this.pesawat.info());
+        System.out.println("----------------------------------------------------");
+        System.out.println("Total keuntungan Armada Kapal\t: "+hargaTotalKapal());
+        System.out.println("Total keuntungan Armada Pesawat\t: "+hargaTotalPesawat());
+        System.out.println("----------------------------------------------------");
     }
 }
